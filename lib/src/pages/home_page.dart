7348e6cbdac1a37c8942d931e2fa9ba99ccaa7a3
Emanuelguantay <emanuel.guantay92@gmail.com>
2021-06-15 23:45:43 -0300

    Pages maps & address

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,16 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:qreader_app/src/pages/address_page.dart';
+import 'package:qreader_app/src/pages/maps_page.dart';
  
 void main() => runApp(HomePage());
  
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Container(
-            child: Text('Hello HomePage'),
-          ),
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: (){},
+          )
+        ],
+      ),
+        body: _callPage(currentIndex),
+        bottomNavigationBar: _createBottomNavigatorBar(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: _floatingActionButton(),
+    );
+  }
+
+  _floatingActionButton(){
+    return FloatingActionButton(
+      child: Icon(Icons.filter_center_focus),
+      onPressed: (){},
+      backgroundColor: Theme.of(context).primaryColor,
+    );
+  }
+
+  _callPage(int page){
+    switch(page){
+      case 0:
+        return MapsPage();
+      case 1:
+        return AddressPage();
+      default:
+        return MapsPage();
+    }
+  }
+
+  _createBottomNavigatorBar(){
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: "Mapas",
         ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          label: "Direcciones",
+        )
+      ]
     );
   }
 }
