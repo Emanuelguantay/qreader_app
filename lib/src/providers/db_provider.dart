5f5876a0ca7526e12df4fee84f72898c3783f7de
Emanuelguantay <emanuel.guantay92@gmail.com>
2021-06-17 01:26:28 -0300

    CreateDB

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:qreader_app/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -20,7 +26,43 @@ class DBProvider {
   }
 
   initDB() async{
+    //crear una instancia de la base de datos
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentDirectory.path, 'ScansDB.db');
 
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db){},
+      onCreate: (Database db, int version) async{
+        await db.execute(
+          'CREATE TABLE Scans ('
+            'id INTEGER PRIMARY KEY,'
+            'type TEXT,'
+            'value TEXT'
+          ')'
+        );
+      }
+    );
+  }
+
+  //Create Registes
+  newScanRaw(ScanModel newScan) async{
+    final db = await database;
+
+    final res = await db.rawInsert(
+      "INSERT INTO Scans ( id, type, value) "
+      "VALUES ( ${newScan.id}, '${newScan.type}', '${newScan.value}' )"
+    );
+
+    return res;
+  }
+
+  newScan(ScanModel newScan) async{
+    final db = await database;
+    
+    final res = db.insert('Scans', newScan.toJson());
+    return res;
   }
 
 }
